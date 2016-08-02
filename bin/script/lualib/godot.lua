7494a0e589abaa97c29dadf836b5fd9d58d4930c
sanikoyes <sanikoyes@163.com>
2016-08-02 18:11:48 +0800

    update godot engine tscn parser

--- bin/script/lualib/godot.lua
+++ bin/script/lualib/godot.lua
@@ -1,6 +1,9 @@
 local tscn = require "script.lualib.tscn"
 local utils = require "script.lualib.utils"
 
+--------------------------------------------------------------------------------
+-- 解析tscn gdscene数据
+--------------------------------------------------------------------------------
 local function parse_gdscene(text)
     -- local fp = io.open(path, "rb")
     -- local text = fp:read "*all"
@@ -23,6 +26,17 @@ local function parse_gdscene(text)
             local name = self.children[index]
             return scene.nodes[name]
         end
+    end
+
+    function mt:get_property(name)
+        local prop = self[name]
+        if type(prop) == "table" then
+            if prop.type == "SubResource" then
+                local id = prop.props[1]
+                return scene.sub_resources[id]
+            end
+        end
+        return prop
     end
 
     local parse_prims, parse_tags, get_prim
@@ -105,7 +119,153 @@ local function parse_gdscene(text)
     return scene
 end
 
+--------------------------------------------------------------------------------
+-- 将table打包成godot的binary数据
+--------------------------------------------------------------------------------
+local Type = {
+    NIL = 0,
+    -- atomic types
+    BOOL = 1,
+    INT = 2,
+    REAL = 3,
+    STRING = 4,
+    -- math types
+    VECTOR2 = 5,
+    RECT2 = 6,
+    VECTOR3 = 7,
+    MATRIX32 = 8,
+    PLANE = 9,
+    QUAT = 10,
+    AABB = 11,
+    MATRIX3 = 12,
+    TRANSFORM = 13,
+    -- misc types
+    COLOR = 14,
+    IMAGE = 15,
+    NODE_PATH = 16,
+    _RID = 17,
+    OBJECT = 18,
+    INPUT_EVENT = 19,
+    DICTIONARY = 20,
+    ARRAY = 21,
+    -- arrays
+    RAW_ARRAY = 22,
+    INT_ARRAY = 23,
+    REAL_ARRAY = 24,
+    STRING_ARRAY = 25,
+    VECTOR2_ARRAY = 26,
+    VECTOR3_ARRAY = 27,
+    COLOR_ARRAY = 28,
+    VARIANT_MAX = 29,
+}
+
+local function encode_variant(v)
+    local num_keys = 0
+    local function var2type(v)
+        local t = type(v)
+        if t == "table" then
+            local i = 1
+            local is_array = true
+            for k,_ in pairs(v) do
+                if k ~= i then
+                    is_array = false
+                end
+                i = i + 1
+                num_keys = num_keys + 1
+            end
+            if not is_array then
+                if num_keys == 2 then
+                    if v.x and v.y then
+                        return Type.VECTOR2
+                    end
+                end
+            end
+            return is_array and Type.ARRAY or Type.DICTIONARY
+        elseif t == "boolean" then
+            return Type.BOOL
+        elseif t == "number" then
+            if math.ceil(v) == v then
+                return Type.INT
+            else
+                return Type.REAL
+            end
+        elseif t == "string" then
+            return Type.STRING
+        else
+            error("Unsupported type: " .. t)
+        end
+    end
+
+    local function encode_uint16(v)
+        return string.pack("<I2", v)
+    end
+
+    local function encode_uint32(v)
+        return string.pack("<I4", v)
+    end
+
+    local function encode_uint64(v)
+        return string.pack("<I8", v)
+    end
+
+    local function encode_float(v)
+        return string.pack("<f", v)
+    end
+
+    local function encode_double(v)
+        return string.pack("<d", v)
+    end
+
+    local function encode_cstring(v)
+        return string.pack("z", v)
+    end
+
+    local function encode_string(v)
+        local s = string.pack("<s4", v)
+        -- pad
+        while (#s % 4) ~= 0 do
+            s = s .. '\0'
+        end
+        return s
+    end
+
+    local buf = ""
+    local t = var2type(v)
+    buf = buf .. encode_uint32(t)
+
+    if t == Type.NIL then
+    elseif t == Type.BOOL then
+        buf = buf .. encode_uint32(v and 1 or 0)
+    elseif t == Type.INT then
+        buf = buf .. encode_uint32(v)
+    elseif t == Type.REAL then
+        buf = buf .. encode_double(v)
+    elseif t == Type.STRING then
+        buf = buf .. encode_string(v)
+    elseif t == Type.VECTOR2 then
+        buf = buf 
+            .. encode_float(v.x)
+            .. encode_float(v.y)
+    elseif t == Type.DICTIONARY then
+        local tokens = {}
+        table.insert(tokens, encode_uint32(num_keys | 0x80000000))
+        for k,v in pairs(v) do
+            table.insert(tokens, encode_variant(k))
+            table.insert(tokens, encode_variant(v))
+        end
+        buf = buf .. table.concat(tokens)
+    elseif t == Type.ARRAY then
+        local tokens = {}
+        table.insert(tokens, encode_uint32(num_keys | 0x80000000))
+        for _,v in pairs(v) do
+            table.insert(tokens, encode_variant(v))
+        end
+        buf = buf .. table.concat(tokens)
+    end
+    return buf
+end
 
 return {
     parse_gdscene = parse_gdscene,
+    encode_variant = encode_variant,
 }
